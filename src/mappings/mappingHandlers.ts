3c01fae35793b1c6917ce6323f8d32c2c065d94e
Denis-at-612441457433 <boykas@ciklum.com>
2022-01-23 13:28:20 +0200

    init project and Staking Reward indexing logic

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,19 @@
+import {SubstrateEvent} from "@subql/types";
+import {StakingReward} from "../types";
+import {Balance} from "@polkadot/types/interfaces";
+
+
+export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [account, newReward]}} = event;
+    //Create the stakingReward entity
+    const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
+    //Set all properties to entity
+    entity.account = account.toString();
+    entity.balance = (newReward as Balance).toBigInt();
+    entity.date = event.block.timestamp;
+    entity.blockHeight = event.block.block.header.number.toNumber();
+    await entity.save();
+}
+
+
+
